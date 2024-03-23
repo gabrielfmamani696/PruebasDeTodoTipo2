d60182cfb6af1587636b61d0167dffabdd236bd3
gabrielfmamani696 <gabrielfmamanicatari@gmail.com>
2024-03-23 17:03:08 -0400

    4th services_pivot_informacion

--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicioFamiliarService } from '../servicio-familiar.service';
 
 @Component({
   selector: 'app-padre',
   templateUrl: './padre.component.html',
   styleUrl: './padre.component.css'
 })
-export class PadreComponent {
+export class PadreComponent implements OnInit{
+
+  nombre?: string;
+
+  constructor(
+    private _servicioFamiliar: ServicioFamiliarService
+  ){}
+
+  ngOnInit(): void {
+    this._servicioFamiliar.setHermanoGrande('Juan');
+    this.nombre=this._servicioFamiliar.getHermanoGrande();
+  }
+
+  saludar(){
+    this._servicioFamiliar.saludar(this._servicioFamiliar.getHermanoPeque() || '')
+  }
+
+  preguntar(){
+    console.log(this._servicioFamiliar.preguntarPorHijo());
+    
+  }
 
   mensajeRecibido: string = '';
 
