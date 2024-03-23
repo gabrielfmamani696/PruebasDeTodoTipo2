d3444aed5b8c7c8b5c1066c870a02a6f561f5abd
gabrielfmamani696 <gabrielfmamanicatari@gmail.com>
2024-03-23 17:53:21 -0400

    5th directivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { PadreComponent } from './padre/padre.component';
 import { HijoComponent } from './hijo/hijo.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HermanoComponent } from './hermano/hermano.component';
+import { EstiloHermanosDirective } from './estilo-hermanos.directive';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HermanoComponent } from './hermano/hermano.component';
     AppComponent,
     PadreComponent,
     HijoComponent,
-    HermanoComponent
+    HermanoComponent,
+    EstiloHermanosDirective
   ],
   imports: [
     BrowserModule,
